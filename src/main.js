e4a14d6d7c7dd59223c87631a02db3675c7cbec6
mingmingzhang123 <1138195675@qq.com>
2022-09-19 15:52:24 +0800

    chore(custom): 安装elment-ui以及axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from "vue";
+// 引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+// 注册element-ui
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,13 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <p>
-      hello
-   word</p>
+    <el-button icon="el-icon-search" circle></el-button>
+  <el-button type="primary" icon="el-icon-edit" circle></el-button>
+  <el-button type="success" icon="el-icon-check" circle></el-button>
+  <el-button type="info" icon="el-icon-message" circle></el-button>
+  <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+  <el-button type="danger" icon="el-icon-delete" circle></el-button>
+</el-row>
   </div>
 </template>
 
